5e2376f68d82371b73aa4272a4b73eed292b6dbe
a2zrealestateinvestments-prog <a2zrealestate.investments@gmail.com>
2026-04-14 17:04:06 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/LUMA--Reconecta/', // <--- Asegúrate de que esto tenga el nombre de tu repositorio
 })
